75621592b7549e5090226757a493c7ba3182047c
cesareuseche <cesareuseche@gmail.com>
2021-03-29 20:20:11 -0400

    made video display as mirror and made some changes to the server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const sever = app.listen(port, () => console.log(`Server started on port ${port}`))
+
+app.use('/', express.static('public'))
+app.get("/server", (req, res) => res.send('server its working on port 3000 as well'))
